bb517d7cd84c0fec34f3d764e247374194fd9810
vanjajs <vanjolina@gmail.com>
2018-06-16 19:09:28 +0200

    Add pagination

--- src/App.js
+++ src/App.js
@@ -2,40 +2,66 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 
-import { Nav, NavItem, NavLink, Button, Form, FormGroup, Input } from 'reactstrap';
+import { Nav, NavItem, Button, Form, FormGroup, Input } from 'reactstrap';
 
 import MovieList from './components/MovieList';
 import MovieInfo from './components/MovieInfo';
+import Spinner from './components/Spinner';
 
 const API_KEY = '7becba0';
+const PER_PAGE = 10;
 
 class App extends Component {
   state = {
+    searchTerm: '',
     movies: [],
     total: 0,
     showInfo: false,
-    info: {}
+    info: {},
+    page: 0,
+    totalPages: 0,
+    loading: false
   }
 
   searhMovie = (event) => {
     event.preventDefault();
-    console.log(event.target.title.value);
-    axios.get('http://www.omdbapi.com/?apikey=' + API_KEY + '&s=' + event.target.title.value)
+    this.setState({ 
+      searchTerm: event.target.title.value, 
+      movies: [],
+      total: 0,
+      page: 0,
+      totalPages: 0
+    });
+    this.loadMovies(event.target.title.value);
+  }
+  
+  loadMovies = (searchTerm) => {
+    this.setState({ loading: true });
+    const page = this.state.page + 1;
+    axios.get('http://www.omdbapi.com/?apikey=' + API_KEY + '&s=' + searchTerm + '&page=' + page)
     .then(response => {
       if (response.data.Error) {
         alert(response.data.Error);
         this.setState({
           movies: [],
-          total: 0
+          total: 0,
+          loading: false
         });
       }
       else {
-        this.setState({
-          movies: response.data.Search,
-          total: response.data.totalResults
+        const totalPages = Math.floor(response.data.totalResults / PER_PAGE);
+        this.setState(prevState => {
+          return {
+            movies: prevState.movies.concat(response.data.Search),
+            total: response.data.totalResults,
+            totalPages: totalPages,
+            page: page,
+            loading: false
+          }
         });
       }
     })
+
   }
 
   showInfoHandler = (id) => {
@@ -62,6 +88,14 @@ class App extends Component {
       info = <MovieInfo data={this.state.info} close={this.closeInfoHandler} />
     }
 
+    let nextLink = null;
+    if (this.state.page < this.state.totalPages) {
+      nextLink = <Button color="primary" size="lg" block onClick={() => this.loadMovies(this.state.searchTerm)}>Load More</Button>
+    }
+
+    // const spinner = this.state.loading? <Spinner /> : null;
+    const spinner = <Spinner />;
+
     return (
       <div className="App">
         <Nav className="navbar navbar-light bg-light">
@@ -78,6 +112,10 @@ class App extends Component {
         </Nav>  
 
         <MovieList movies={this.state.movies} clicked={this.showInfoHandler} />
+
+        {spinner}
+
+        {nextLink}
 
         {info}
 
---
+++ src/components/Spinner.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const spinner = () => (
+  <div className="Loader"><div></div><div></div><div></div><div></div></div>
+);
+
+export default spinner;
